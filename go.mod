f026a82b31abfd60f684c1c328e242dc10f3751b
Nathan <92413483+n8zone@users.noreply.github.com>
2025-11-12 19:27:04 -0500

    Hello, world

---
+++ go.mod
@@ -0,0 +1,5 @@
+module pet-everyone
+
+go 1.24.9
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	filepathRoot := os.Getenv("FILEPATH_ROOT")
+	if filepathRoot == "" {
+		log.Fatal("FILEPATH_ROOT environment variable not set")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	mux := http.NewServeMux()
+	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app", appHandler)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on http://localhost:%s/app\n", port)
+	log.Fatal(srv.ListenAndServe())
+
+}
