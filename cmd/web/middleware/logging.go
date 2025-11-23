4c482ffe6b61e1ae8215ebf9c0820e1b15284887
Nathan <92413483+n8zone@users.noreply.github.com>
2025-11-22 22:21:16 -0500

    switch to new closure method

--- cmd/web/middleware/logging.go
+++ cmd/web/middleware/logging.go
@@ -1,27 +1,15 @@
 package middleware
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"time"
+	"pet-everyone/cmd/web/application"
 )
 
-func LoggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("|||||| REQUEST LOG  ||||||")
-		log.Println("METHOD: ", r.Method)
-		log.Println("URL: ", r.URL)
-		log.Println("REMOTE_ADDR: ", r.RemoteAddr)
-		log.Println("USER_AGENT: ", r.UserAgent())
-		log.Println("PARAMS: ", r.URL.Query(), "")
-		log.Println("HTTP VERSION: ", r.Proto)
-
-		start := time.Now()
-		next.ServeHTTP(w, r)
-		dur := time.Since(start)
-
-		log.Printf("Request took %v", dur)
-		fmt.Println("|||||| END OF REQUEST LOG  ||||||")
+func Logging(app *application.Config) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			app.Logger().Info("Request received", "method", r.Method, "url", r.URL)
+			next.ServeHTTP(w, r)
+		})
 	}
 }
