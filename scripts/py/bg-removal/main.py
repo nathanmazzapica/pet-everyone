5e2937a2d94a6358d0f63a1cc8ffde12e50bd60c
Nathan <92413483+n8zone@users.noreply.github.com>
2025-12-02 18:43:04 -0500

    Python Image Background removal
    Special thanks to https://github.com/withoutbg/withoutbg for providing a free Python package to use their image background removal model

---
+++ scripts/py/bg-removal/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from withoutbg import WithoutBG
+from typing import Union
+import sys
+
+def printerr(v) -> None:
+    print(v, file=sys.stderr)
+
+def trim_ext(target: str) -> str:
+    parts = target.split(".")
+    if len(parts) != 2 or (parts[0] == "" or parts[1] == ""):
+        raise ValueError(f"invalid input string:\n{target}")
+    return parts[0]
+
+def process(target) -> Union[str, None]:
+    """process removes the background of the target image.
+    Returns output_file_name: str if successful
+    otherwise, returns None"""
+    trimmed_target = ""
+    try:
+        trimmed_target = trim_ext(target)
+    except ValueError as e:
+        printerr(f"error processing target file:{e}")
+        return None
+
+    output_file_name = f"{trimmed_target}-trans.png"
+
+    clcoding = WithoutBG.opensource()
+    result = clcoding.remove_background(target)
+
+    try:
+        result.save(output_file_name)
+    except ValueError as ve:
+        printerr(ve)
+        return None
+    except OSError as ose:
+        printerr(ose)
+        return None
+
+    return output_file_name
+
+if __name__ == "__main__":
+    num_args = len(sys.argv)
+
+    if num_args != 2:
+        printerr("invalid number of args")
+        sys.exit(1)   
+
+    target = sys.argv[1]
+
+    res = process(target)
+
+    if res is None:
+        sys.exit(1)
+
+    print(res)
