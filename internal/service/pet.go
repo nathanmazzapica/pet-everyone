7f63cce8e2dbb6fa8bc0176ac8c0a506f4a225c8
Nathan <92413483+n8zone@users.noreply.github.com>
2025-12-11 22:02:01 -0500

    feat: Add basic pet count memory and persistence storage

--- internal/service/pet.go
+++ internal/service/pet.go
@@ -1,27 +1,33 @@
 package service
 
 import (
+	"fmt"
 	"log"
 	"pet-everyone/internal/data/model"
 	"sync"
+	"time"
 )
 
 type PetService struct {
 	petID    string
 	petCount uint64
+	dbQueue  map[string]uint64
 	mu       *sync.RWMutex
 	events   chan<- Event
 	db       *model.PetModel
 }
 
 func NewPetService(petID string, model *model.PetModel, out chan<- Event) *PetService {
-	return &PetService{
+	service := &PetService{
 		petID:    petID,
 		petCount: 0, // will get from db later
+		dbQueue:  make(map[string]uint64),
 		mu:       &sync.RWMutex{},
 		events:   out,
 		db:       model,
 	}
+	service.init()
+	return service
 }
 
 // petEvent is used to send pet count to websocket clients
@@ -29,9 +35,10 @@ type petEvent struct {
 	Ack byte `json:"c"`
 }
 
-func (s *PetService) IncrementPetCount() error {
+func (s *PetService) IncrementPetCount(userID string) error {
 	s.mu.Lock()
 	s.petCount++
+	s.dbQueue[userID]++
 	s.mu.Unlock()
 	petEvent := Event{Type: "pet", Data: petEvent{Ack: 1}}
 	s.events <- petEvent
@@ -57,6 +64,24 @@ func (s *PetService) init() {
 		log.Fatal(err)
 	}
 	s.petCount = uint64(c)
+	go s.persistCountsToDatabase()
+}
+
+func (s *PetService) persistCountsToDatabase() {
+	for {
+		time.Sleep(4 * time.Second)
+		fmt.Println("persisting counts to database")
+		s.mu.RLock()
+		defer s.mu.RUnlock()
+		for userID, count := range s.dbQueue {
+			fmt.Println("persisting count for user", userID, "to database")
+			err := s.db.UpdatePetCount(s.petID, userID, count)
+			if err != nil {
+				fmt.Println("PETID: ", s.petID)
+				fmt.Println("error persisting count to database:", err)
+			}
+		}
+	}
 }
 
 func (s *PetService) Is() {}
