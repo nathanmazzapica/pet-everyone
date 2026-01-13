cb01a58619583797a13a4540dc231230ae429c15
Nathan <92413483+n8zone@users.noreply.github.com>
2026-01-12 21:23:53 -0500

    feat: add visitor model and guest/user pet counts

--- internal/data/model/user.go
+++ internal/data/model/user.go
@@ -99,9 +99,9 @@ func (u *UserModel) GetByEmail(email string) (*User, error) {
 	return &user, err
 }
 
-func GetPetCountByPetID(db *sql.DB, id *uuid.UUID, petID *string) (int64, error) {
+func (u *UserModel) GetPetCountByPetID(userID *uuid.UUID, petID *string) (int64, error) {
 	query := `SELECT COALESCE(SUM(click_count), 0) FROM UserPetsClickCount WHERE pet_id = $1 AND user_id = $2;`
-	row := db.QueryRow(query, petID, id)
+	row := u.DB.QueryRow(query, petID, userID)
 
 	var count int64
 	err := row.Scan(&count)
